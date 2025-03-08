e4c6f8aa3b8f2736f925ef46dadd7b4de1d0fd56
navarroceti <129806280+navarroceti@users.noreply.github.com>
2025-03-08 01:43:08 +0000

    Codigo base de mascota

---
+++ Mascota.hpp
@@ -0,0 +1,19 @@
+class Mascota
+{
+private:
+    int vida;
+public:
+    Mascota() {
+        vida = 100;
+    }
+    ~Mascota() {}
+    void Golpear(){
+        vida -= 10;
+    }
+    int LeerVida(){
+        return vida;
+    }
+    void Acariciar(){
+        vida += 10;
+    }
+};
